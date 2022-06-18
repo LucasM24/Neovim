dee58a2a930cfd5401c8f93ce6e701e92b07999c
Lucas <lucas.mendez@est.fi.uncoma.edu.ar>
2022-06-18 00:08:18 -0300

    devicons, popup and plenary added

--- lua/config/packer/plugins.lua
+++ lua/config/packer/plugins.lua
@@ -42,6 +42,9 @@ packer.init {
 return packer.startup(function(use)
   -- My plugins here
   use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "kyazdani42/nvim-web-devicons"
 
   -- Colorschemes
   use "lunarvim/darkplus.nvim"
