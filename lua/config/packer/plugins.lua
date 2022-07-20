020ee2ec5246b624635f6daa79808e2180b1fe52
Lucas Mendez-dev <lucas.mendez@est.fi.uncoma.edu.ar>
2022-07-19 22:34:41 -0300

    Colorizer added

--- lua/config/packer/plugins.lua
+++ lua/config/packer/plugins.lua
@@ -117,6 +117,9 @@ return packer.startup(function(use)
   -- Bufferline
   use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
 
+  --Colorizer
+  use 'norcalli/nvim-colorizer.lua'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
